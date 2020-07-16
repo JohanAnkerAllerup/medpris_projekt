293d380ddcb5f465ad760fe1be07ba50720b0ed6
spakhugger <Johan_allerup@hotmail.com>
2020-07-16 22:40:40 +0200

    functioning webpage for 10 mg contalgin

--- medpris_root/firstUI/views.py
+++ medpris_root/firstUI/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import pandas as pd
+import datetime
 
 # Create your views here.
 def indexPage(request):
@@ -7,5 +8,12 @@ def indexPage(request):
     pris_contalgin = pd.read_excel ("/Users/johanankerallerup/dev/medpris_projekt/Data_medpriser/contalgin10_pris_19_20.xlsx", sheet_name=1)
     pris = pris_contalgin[pris_contalgin.columns[1]].tolist()
     varA = pris_contalgin[pris_contalgin.columns[0]].tolist()
-    context = {"pris" :pris, "varA": varA}
+
+
+    list_datostr = []
+    for i in range(len(varA)):
+        DatoStr = varA[i].strftime("%d-%b-%Y")
+        list_datostr.append(DatoStr)
+
+    context = {"pris" :pris, "list_datostr": list_datostr}
     return(render(request, 'index.html', context))
